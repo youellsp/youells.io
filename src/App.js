d8871bf83e5f63c04fa3fd740207f32fdd5cc85f
youellsp <youells.patrick@gmail.com>
2019-06-23 16:25:21 -0400

    Added Landing page w/coming soon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './patrick_bitmoji.png';
 import './App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <code>
+          I'm Patrick.
+        </code>
+        <code>
+          A <strong>Full Stack</strong> React and Node JS Developer
+        </code>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/patrick-youells-2919a559/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <FontAwesomeIcon size="1.5x" className="font-awesome" icon={faLinkedinIn} />
         </a>
+        <p>
+          <code style={{'font-size': '8px'}}>
+            More coming soon...
+          </code>
+        </p>
       </header>
     </div>
   );
